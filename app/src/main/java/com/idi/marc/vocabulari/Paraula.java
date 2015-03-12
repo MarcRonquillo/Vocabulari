7b812c07e9a2f168917b753e30bd02e19ca5554a
MarcRonquillo <marc.ronq@gmail.com>
2015-03-12 18:58:59 +0100

    classe idioma

--- app/src/main/java/com/idi/marc/vocabulari/Paraula.java
+++ app/src/main/java/com/idi/marc/vocabulari/Paraula.java
@@ -5,4 +5,31 @@ package com.idi.marc.vocabulari;
  */
 public class Paraula {
 
+    private static Integer id;
+    private String valor;
+
+    public Paraula(String valor){
+        setValor(valor);
+    }
+
+    /**
+     * Getters i setters
+     * @return
+     */
+    public Integer getId(){
+        return this.id;
+    }
+
+    public String getValor(){
+        return this.valor;
+    }
+
+    public void setId(Integer id){
+        this.id=id;
+    }
+
+    public void setValor(String valor){
+        this.valor=valor;
+    }
+
 }
